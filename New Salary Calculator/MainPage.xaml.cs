01fb4ccff48b1c4923cc375dc4a68f23061d06ca
Aaditya Menon <aaditya@sparker0i.me>
2019-04-20 04:58:30 +0530

    DataGrid Data Structure Configured

--- New Salary Calculator/MainPage.xaml.cs
+++ New Salary Calculator/MainPage.xaml.cs
@@ -18,7 +18,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Sample.Data
 {
-    class TableFiller : ObservableCollection<TableData>
+    /*class TableFiller : ObservableCollection<TableData>
     {
         public TableFiller()
         {
@@ -46,15 +46,9 @@ namespace Sample.Data
                 Email = "user3@nowhere.local"
             });
         }
-    }
+    }*/
 
-    class TableData
-    {
-        public string UserName { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-    }
+    
 }
 
 namespace New_Salary_Calculator
@@ -62,11 +56,27 @@ namespace New_Salary_Calculator
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    
+    public class TableData
+    {
+        public Double Basic { get; set; }
+        public Double Fbp { get; set; }
+        public Double Inh { get; set; }
+        public Double Ars { get; set; }
+        public Double Rent { get; set; }
+        public Double Ins { get; set; }
+        public Double Inv { get; set; }
+        public Double Cur { get; set; }
+    }
+
     public sealed partial class MainPage : Page
     {
+        public List<TableData> Details { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
+            Details = new List<TableData>();
         }
 
         private void TextBox_OnTextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
@@ -77,23 +87,39 @@ namespace New_Salary_Calculator
         private void calculate_click(object sender, RoutedEventArgs e)
         {
             int years = Convert.ToInt32(yearInput.Text) - 1;
-            Double pow = Math.Pow(1.09, years);
-            Double basic = Math.Round(Convert.ToDouble(basicInput.Text) * pow),
-                fbp = Math.Round(basic * Convert.ToDouble(fbpInput.Text) / 100),
-                pf = Math.Round(basic * Convert.ToDouble(pfInput.Text) / 100),
-                grat = Math.Round(basic * Convert.ToDouble(gratInput.Text) / 100);
+            for (int i = 1; i <= years; ++i)
+            {
+                TableData data = new TableData();
+                Double pow = Math.Pow(1.09, years);
+                Double basic = Math.Round(Convert.ToDouble(basicInput.Text) * pow),
+                    fbp = Math.Round(basic * Convert.ToDouble(fbpInput.Text) / 100),
+                    pf = Math.Round(basic * Convert.ToDouble(pfInput.Text) / 100),
+                    grat = Math.Round(basic * Convert.ToDouble(gratInput.Text) / 100);
 
-            Double inh = Math.Floor(basic + fbp), ars = Math.Round(fbp + basic + pf + grat);
+                Double inh = Math.Floor(basic + fbp), ars = Math.Round(fbp + basic + pf + grat);
 
-            Double rent = Math.Round(Convert.ToDouble(rentInput.Text) * 12),
-                food = Math.Round(Convert.ToDouble(foodInput.Text) * pow),
-                investments = Math.Round(Convert.ToDouble(investmentsInput.Text) * pow),
-                ins = 100000;
+                Double rent = Math.Round(Convert.ToDouble(rentInput.Text) * 12),
+                    food = Math.Round(Convert.ToDouble(foodInput.Text) * pow),
+                    investments = Math.Round(Convert.ToDouble(investmentsInput.Text) * pow),
+                    ins = 100000;
 
-            Double cur = inh - rent - food - investments - ins;
-            inhandOutput.Text = Convert.ToString(Math.Round(cur));
-            inhandMonthOutput.Text = Convert.ToString(Math.Round(cur/12));
-            arsOutput.Text = Convert.ToString(ars);
+                Double cur = inh - rent - food - investments - ins;
+
+                data.Basic = basic;
+                data.Inh = inh;
+                data.Inv = investments;
+                data.Ars = ars;
+                data.Rent = rent;
+                data.Cur = cur;
+                data.Ins = ins;
+                data.Fbp = fbp;
+
+                Details.Add(data);
+
+                inhandOutput.Text = Convert.ToString(Math.Round(cur));
+                inhandMonthOutput.Text = Convert.ToString(Math.Round(cur / 12));
+                arsOutput.Text = Convert.ToString(ars);
+            }
         }
     }
 }
